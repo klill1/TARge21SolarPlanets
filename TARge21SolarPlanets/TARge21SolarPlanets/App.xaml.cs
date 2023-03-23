722229e3e919ef5b7b9f8d26a36001f6af787586
Opilane TTHK <opilane@tthk.ee>
2023-03-23 15:01:58 +0200

    Windows size and StartPage

--- TARge21SolarPlanets/TARge21SolarPlanets/App.xaml.cs
+++ TARge21SolarPlanets/TARge21SolarPlanets/App.xaml.cs
@@ -1,11 +1,39 @@
-﻿namespace TARge21SolarPlanets;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Views;
+using Windows.Graphics;
+#endif
+
+
+namespace TARge21SolarPlanets;
 
 public partial class App : Application
 {
-	public App()
+    const int WindowWidth = 540;
+    const int WindowHeight = 1000;
+    public App()
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+#if WINDOWS
+
+		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+		{
+			var mauiWindow = handler.VirtualView;
+			var nativeWindow = handler.PlatformView;
+			nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+			AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+			appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+
+		});
+		
+
+
+	
+#endif
+        MainPage = new NavigationPage(new StartPage());
 	}
 }
